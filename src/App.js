af3e0fe9bd3e01eca97eb40e60138fc3e20c134f
rishabhscross <parammittal16@gmail.com>
2019-01-09 00:26:31 +0530

    Test first deploy

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header ">
           <img src={logo} alt="logo" className="App-logo" />
-          <h1 className="App-title">Welcome to Your MDB React App</h1>
+          <h1 className="App-title">Welcom to Your MDB React App</h1>
         </header>
         <p className="mb-2">The application is configured and ready to import our components.</p>
         <Button href="https://mdbootstrap.com/react/" target="blank" color="light-blue"><strong>Check out our docs!</strong></Button>
